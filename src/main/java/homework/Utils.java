aa9bf32153108f0512219db2c36b81d2bbf930e8
Olga <littleshef1306@gmail.com>
2025-02-03 16:11:12 +0200

    commit

--- src/main/java/homework/Utils.java
+++ src/main/java/homework/Utils.java
@@ -2,7 +2,7 @@ package homework;
 
 
 public class Utils {
-    private static final String URL = "http://localhost:8088/ChatServerHw_war_exploded/";
+    private static final String URL = "http://localhost:8088/ChatServerHw_war_exploded";
     private static final int PORT = 8088;
 
     public static String getURL() {
